02bb132eb1088e400e9ce2a8a8e8c9d4d869b9fb
LuDon89 <daudonlucas@gmail.com>
2023-04-28 14:19:31 +0200

    affichage données ok

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -2,28 +2,46 @@ import '../styles/recipe.css';
 
 import Data from "./data/recettes.json";
 
-export function Recipe ({ recipeName, recipeServings, recipeDescription }) {
-  return (
-    <div className="recipeCard">
-      <h2 className="recipeName">
-        {recipeName}
-      </h2>
-      <img src="https://picsum.photos/300/200" alt="" />
-      <p>
-        Pour {recipeServings} personnes
-      </p>
-      <p>
-        {recipeDescription}
-      </p>
-    </div>
-  );
-}
+// export function Recipe ({ recipeName, recipeServings, recipeDescription }) {
+//   return (
+//     <div className="recipeCard">
+//       <h2 className="recipeName">
+//         {recipeName}
+//       </h2>
+//       <img src="https://picsum.photos/300/200" alt="" />
+//       <p>
+//         Pour {recipeServings} personnes
+//       </p>
+//       <p>
+//         {recipeDescription}
+//       </p>
+//     </div>
+//   );
+// }
 
 export function RecipeData () {
   return (
-    <div>
-      <p>TEST</p>
-    </div>
+    <>
+      {
+        Data.map(data => {
+          return (
+            <div className="recipeCard">
+              <h2 className="recipeName">
+                {data.name}
+              </h2>
+              <img src="https://picsum.photos/300/200" alt="" />
+              <p>
+                Pour {data.servings} personnes
+              </p>
+              <p>
+                {data.description}
+              </p>
+            </div>
+          );
+        })
+      }
+
+    </>
   );
 }
 
--- src/pages/recettes.jsx
+++ src/pages/recettes.jsx
@@ -3,23 +3,17 @@ import React from 'react';
 // Components importations
 import Titre from '../components/Titre';
 import Content from '../components/Content';
-import { Recipe } from '../components/Recipe';
 import { RecipeData } from '../components/Recipe';
 
 const Recettes = () => {
-    return (
-        <div>
-            <Titre text="Recettes" />
-            <Content>
-                <Recipe
-                    recipeName="Soupalognon"
-                    recipeServings="2"
-                    recipeDescription="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim."
-                />
-                <RecipeData />
-            </Content>
-        </div>
-    );
+  return (
+    <>
+      <Titre text="Recettes" />
+      <Content>
+        <RecipeData />
+      </Content>
+    </>
+  );
 };
 
 export default Recettes;
