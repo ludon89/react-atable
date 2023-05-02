bb6e58cbb4cf4375394f0750f3349cbadc07db28
LuDon89 <daudonlucas@gmail.com>
2023-05-02 15:31:38 +0200

    setup conditions affichage message d'erreur

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -5,7 +5,7 @@ import '../styles/searchbar.css';
 
 import Data from "./data/recettes.json";
 
-export function RecipeData () { // TODO : affichage des ustensiles en liste
+export function RecipeData () {
   const [searchInput, setSearchInput] = useState("");
   const [filteredResults, setFilteredResults] = useState([]);
 
@@ -21,9 +21,13 @@ export function RecipeData () { // TODO : affichage des ustensiles en liste
     } else {
       setFilteredResults(Data);
     }
-
-
   };
+
+  console.log(filteredResults);
+
+  if (condition) {
+
+  }
 
   return (
     <>
@@ -38,108 +42,109 @@ export function RecipeData () { // TODO : affichage des ustensiles en liste
         </form>
       </div>
 
-      {searchInput.length > 1 ? (
-        filteredResults.map(item => {
-          return (
-            <div className="recipeCard" key={item.id}>
-              <h2 className="recipeName">
-                {item.name}
-              </h2>
-              <img src={process.env.PUBLIC_URL + "/assets/img/" + item.name + ".jpg"}
-                alt="photo de la recette"
-                className='recipeImage'
-              />
-              <p>
-                Pour {item.servings} personnes
-              </p>
-              <br />
+      {
+        searchInput.length > 1 ? (
+          filteredResults.map(item => {
+            return (
+              <div className="recipeCard" key={item.id}>
+                <h2 className="recipeName">
+                  {item.name}
+                </h2>
+                <img src={process.env.PUBLIC_URL + "/assets/img/" + item.name + ".jpg"}
+                  alt="photo de la recette"
+                  className='recipeImage'
+                />
+                <p>
+                  Pour {item.servings} personnes
+                </p>
+                <br />
 
-              <h3>Ingrédients</h3>
-              {item.ingredients.map(subitem => {
-                return (
-                  <ul key={subitem.ingredient}>
-                    <li>
-                      {subitem.ingredient} : {subitem.quantity} {subitem.unit}
-                    </li>
-                  </ul>
-                );
-              })}
-              <br />
+                <h3>Ingrédients</h3>
+                {item.ingredients.map(subitem => {
+                  return (
+                    <ul key={subitem.ingredient}>
+                      <li>
+                        {subitem.ingredient} : {subitem.quantity} {subitem.unit}
+                      </li>
+                    </ul>
+                  );
+                })}
+                <br />
 
-              <h3>Instructions</h3>
-              <p>
-                {item.description}
-              </p>
-              <br />
+                <h3>Instructions</h3>
+                <p>
+                  {item.description}
+                </p>
+                <br />
 
-              <h3>Appareil requis</h3>
-              <ul>
-                <li>{item.appliance}</li>
-              </ul>
-              <br />
-
-              <h3>Ustensiles requis</h3>
-              {item.ustensils.map(ustensils =>
-                <ul key={ustensils}>
-                  <li>{ustensils}</li>
+                <h3>Appareil requis</h3>
+                <ul>
+                  <li>{item.appliance}</li>
                 </ul>
-              )}
+                <br />
 
-            </div>
-          );
-        })
-      ) : (
-        Data.map(item => {
-          return (
-            <div className="recipeCard" key={item.id}>
-              <h2 className="recipeName">
-                {item.name}
-              </h2>
-              <img src={process.env.PUBLIC_URL + "/assets/img/" + item.name + ".jpg"}
-                alt="photo de la recette"
-                className='recipeImage'
-              />
-              <p>
-                Pour {item.servings} personnes
-              </p>
-              <br />
-
-              <h3>Ingrédients</h3>
-              {item.ingredients.map(subitem => {
-                return (
-                  <ul key={subitem.ingredient}>
-                    <li>
-                      {subitem.ingredient} : {subitem.quantity} {subitem.unit}
-                    </li>
+                <h3>Ustensiles requis</h3>
+                {item.ustensils.map(ustensils =>
+                  <ul key={ustensils}>
+                    <li>{ustensils}</li>
                   </ul>
-                );
-              })}
-              <br />
+                )}
+              </div>
+            );
+          })
+        ) : ( // si 0 ou 1 caractère dans le champ de recherche, on affiche toutes les données
+          Data.map(item => {
+            return (
+              <div className="recipeCard" key={item.id}>
+                <h2 className="recipeName">
+                  {item.name}
+                </h2>
+                <img src={process.env.PUBLIC_URL + "/assets/img/" + item.name + ".jpg"}
+                  alt="photo de la recette"
+                  className='recipeImage'
+                />
+                <p>
+                  Pour {item.servings} personnes
+                </p>
+                <br />
 
-              <h3>Instructions</h3>
-              <p>
-                {item.description}
-              </p>
-              <br />
+                <h3>Ingrédients</h3>
+                {item.ingredients.map(subitem => {
+                  return (
+                    <ul key={subitem.ingredient}>
+                      <li>
+                        {subitem.ingredient} : {subitem.quantity} {subitem.unit}
+                      </li>
+                    </ul>
+                  );
+                })}
+                <br />
 
-              <h3>Appareil requis</h3>
-              <ul>
-                <li>{item.appliance}</li>
-              </ul>
-              <br />
+                <h3>Instructions</h3>
+                <p>
+                  {item.description}
+                </p>
+                <br />
 
-              <h3>Ustensiles requis</h3>
-              {item.ustensils.map(ustensils =>
-                <ul key={ustensils}>
-                  <li>{ustensils}</li>
+                <h3>Appareil requis</h3>
+                <ul>
+                  <li>{item.appliance}</li>
                 </ul>
-              )}
+                <br />
 
-            </div>
-          );
-        })
+                <h3>Ustensiles requis</h3>
+                {item.ustensils.map(ustensils =>
+                  <ul key={ustensils}>
+                    <li>{ustensils}</li>
+                  </ul>
+                )}
 
-      )}
+              </div>
+            );
+          })
+
+        )
+      }
 
     </>
   );
